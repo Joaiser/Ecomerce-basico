185ff656cf19b06669b75340fb7e5eef27444913
Joaiser <aitor.v@hotmail.com>
2024-03-25 03:10:06 +0100

    Cambios hechos en el css, añadido nav responsive, faltan colores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { Filters } from './Filters.jsx';
 import { CartIcon } from './icons.jsx';
+import './header.css';
 
 export function Header({ changeFilters }) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const handleFilterClick = () => {
+        setIsFiltersOpen(!isFiltersOpen);
+    }
+
     return (
+        <>
         <header>
             <h1>Tienda <CartIcon /></h1>
-            <Filters onChange={changeFilters} />
+            <button className="menu-icon" onClick={handleMenuClick}>☰</button>
+            
+            <nav>
+                <ul className={isMenuOpen ? 'open' : ''}>
+                <li><a href="/">Inicio</a></li>
+                    <li><a href="/productos">Productos</a></li>
+                    <li><a href="/intercambios">Intercambios</a></li>
+                    <li><a className="filter-icon" onClick={handleFilterClick}>Filtrar Productos</a></li>
+                    <li><a href="/Foro">Foro</a></li>
+                    <li><a href="/nosotros">Nosotros</a></li>
+                    <li><a href="/contacto">Contacto</a></li>
+                </ul>
+            </nav>
+            <div className={`filters ${isFiltersOpen ? 'open' : ''}`}>
+                <Filters onChange={changeFilters} />
+            </div>
         </header>
+        </>
     );
 }
