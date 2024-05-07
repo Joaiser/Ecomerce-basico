286eb2f06d9c3ed482c30aa6ee58ca364ac0604a
Joaiser <aitor.v@hotmail.com>
2024-05-07 21:06:33 +0200

    seccion de seleccion hecha, falta acabarla + el resto

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -37,11 +37,11 @@ export const useCarousel = (initialIndex = 0) => {
 export function Blog() {
     const { carouselRef, nextSlide, prevSlide } = useCarouselFromHooks();
     const articles = [
-        { title: 'Diferencias entre pantallas OLED, AMOLED, QLED y NanoCell: ¿cuál es mejor',
+        { title: 'Diferencias entre pantallas OLED, AMOLED, QLED y NanoCell: ¿cuál es mejor?',
           content: 'En este artículo te explicamos las diferencias entre las tecnologías de pantallas OLED, AMOLED, QLED y NanoCell, y te ayudamos a elegir la mejor para ti.', 
          imgSrc: 'static/img/tipos-de-pantallas.webp',
           link: '#' },
-        { title: '¿Qué es una placa base y cuál es su función?',
+        { title: '¿Qué es una placa base y cuál es su función? Pulsa click para saber más',
          content: '¿Qué es una placa base y cuál es su función? En este artículo te explicamos todo lo que necesitas saber sobre este componente esencial de tu ordenador.', 
          imgSrc: 'static/img/que-es-placa-base.webp',
           link: '#' },
@@ -49,15 +49,15 @@ export function Blog() {
          content: 'En este artículo te explicamos las diferencias entre los distintos tipos de switch de teclado mecánico, y te ayudamos a elegir el mejor para ti.', 
          imgSrc: 'static/img/tecladoswitchblue.webp',
           link: '#'},
-        { title: 'Las mejores sillas erogonómicas para trabajar desde casa',
+        { title: 'Las mejores sillas erogonómicas para trabajar desde casa de manera remota y cómoda',
           content: 'En este artículo te recomendamos las mejores sillas ergonómicas para trabajar desde casa, y te explicamos cómo elegir la mejor para ti.', 
           imgSrc: 'static/img/sillas-ergonomicas-1.webp',
           link: '#'},
-        { title: 'Cómo trabjar con dos pantallas a la vez conectadas a un portátil',
+        { title: 'Cómo trabajar con dos pantallas a la vez conectadas a un portátil. Pulsa click para saber más',
           content: 'En este artículo te explicamos cómo trabajar con dos pantallas a la vez conectadas a un portátil, y te damos algunos consejos para ser más productivo.', 
           imgSrc: 'static/img/monitores-para-trabajar.webp',
           link: '#'},
-        { title: 'Las mejores sillas gamimg de alta gama',
+        { title: 'Las mejores sillas gamimg de alta gama. Pulsa click para saber más',
           content: 'En este artículo te recomendamos las mejores sillas gaming de alta gama, y te explicamos cómo elegir la mejor para ti.', 
           imgSrc: 'static/img/mejores-sillas-gaming.webp',
           link: '#'}
--- src/components/selecciontop.jsx
+++ src/components/selecciontop.jsx
@@ -1,14 +1,14 @@
-
+import './selecciontop.css';
 import { useState, useRef, useLayoutEffect } from 'react';
-import { useCarousel as useCarouselFromHooks } from '../hooks/carrousel';
 
-export const useCarousel = (initialIndex = 0) => {
+
+export const useCarousel = (productsToShow, initialIndex = 0) => {
     const carouselRef = useRef(null);
     const [currentIndex, setCurrentIndex] = useState(initialIndex);
 
     const nextSlide = () => {
-        if (currentIndex < 5) { 
-            setCurrentIndex(currentIndex + 1);
+        if (currentIndex < productsToShow.length - 4) { 
+            setCurrentIndex(currentIndex + 1); // Cambiado de 2 a 1
         } else {
             setCurrentIndex(0); // Vuelve al inicio cuando llega al final
         }
@@ -18,15 +18,19 @@ export const useCarousel = (initialIndex = 0) => {
         if (currentIndex > 0) {
             setCurrentIndex(currentIndex - 1);
         } else {
-            setCurrentIndex(5); // Vuelve al final cuando llega al inicio
+            setCurrentIndex(productsToShow.length - 1);
         }
     }
+    
 
     useLayoutEffect(() => {
         const updateCarousel = () => {
-            const slideWidth = carouselRef.current.children[0].getBoundingClientRect().width;
-            const transformValue = -currentIndex * slideWidth;
-            carouselRef.current.style.transform = `translateX(${transformValue}px)`;
+            if (carouselRef.current && carouselRef.current.children[0]) {
+                const slideWidth = carouselRef.current.children[0].getBoundingClientRect().width;
+                const transformValue = -currentIndex * slideWidth;
+                carouselRef.current.style.transform = `translateX(${transformValue}px)`;
+                carouselRef.current.style.transition = 'transform 0.5s ease-in-out'; // Agrega esta línea
+            }
         }
         updateCarousel();
     }, [currentIndex]);
@@ -36,50 +40,143 @@ export const useCarousel = (initialIndex = 0) => {
 
 
 export function SelectionTop(){
-    const { carouselRef, nextSlide, prevSlide } = useCarousel();
-    const articles = [
-        // Agrega algunos objetos de prueba para verificar si el carrusel se renderiza correctamente
-        { link: "#", imgSrc: "ruta/a/tu/imagen.jpg", title: "Título del artículo", content: "Contenido del artículo" },
-        // Agrega más objetos de artículo aquí...
+    const [selectedCategory, setSelectedCategory] = useState('Todos');
+    const productsToShow = [
+        {
+            link: '/product/1',
+            image: '/static/img/imgpc/1710-alurin-go-start-intel-pentium-n4020-8gb-256gb-ssd-156-comprar.webp',
+            title: 'Ofertas especiales',
+            price: '100€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/2',
+            image: '/static/img/imgpc/198-lenovo-ideapad-1-15amn7-amd-ryzen-5-7520u-16-gb-512gb-ssd-156.webp',
+            title: 'Reacondicionados',
+            price: '200€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/3',
+            image: '/static/img/imgpc/1703-msi-katana-15-b13vfk-1854xes-intel-core-i7-13700h-16gb-1tb-ssd-rtx-4060-156-fe60fb00-dc49-4c2d-be95-adde65888774.webp',
+            title: 'Concursos',
+            price: '300€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/4',
+            image: '/static/img/imgpc/153-msi-thin-gf63-12uc-688xes-intel-core-i7-12650h-16gb-1tb-ssd-rtx-3050-156.webp',
+            title: 'Blog',
+            price: '400€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/5',
+            image: '/static/img/imgpc/1389-asus-vivobook-15-f1504va-nj766w-intel-core-i7-1355u-16gb-1tb-ssd-156.webp',
+            title: 'Novedades',
+            price: '500€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/6',
+            image: '/static/img/imgpc/1537-acer-aspire-3-a315-44p-amd-ryzen-7-5700u-16gb-512gb-ssd-156.webp',
+            title: 'Pccom',
+            price: '600€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/9',
+            image: '/static/img/imgpc/1686-lenovo-ideapad-slim-3-15iah8-intel-core-i5-12450h-16gb-1tb-ssd-156.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/10',
+            image: '/static/img/imgpc/1703-msi-katana-15-b13vfk-1854xes-intel-core-i7-13700h-16gb-1tb-ssd-rtx-4060-156-fe60fb00-dc49-4c2d-be95-adde65888774.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/11',
+            image: '/static/img/imgpc/153-msi-thin-gf63-12uc-688xes-intel-core-i7-12650h-16gb-1tb-ssd-rtx-3050-156.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/12',
+            image: '/static/img/imgpc/198-lenovo-ideapad-1-15amn7-amd-ryzen-5-7520u-16-gb-512gb-ssd-156.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/13',
+            image: '/static/img/imgpc/more-link.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Ordenadores'
+        },
+        {
+            link: '/product/14',
+            image: '/static/img/imgpc/1710-alurin-go-start-intel-pentium-n4020-8gb-256gb-ssd-156-comprar.webp',
+            title: 'Servicios',
+            price: '800€',
+            category: 'Componentes'
+        }
     ];
+    const { carouselRef, nextSlide, prevSlide } = useCarousel(productsToShow);
+
+    function handleCategoryClick(category, event) {
+        event.preventDefault();
+        setSelectedCategory(category);
+    }
 
     return(
-        <section>
-           <aside>
+        <section className="container">
+           <aside className='aside-container'>
             <h2>Seleccion Top</h2>
             <p>Tus productos favoritos de tecnología 
                 con ofertas y descuentos increíbles.
             </p>
             <a href="">Ver más</a>
            </aside>
-           <article>
-            <ul>
-                <li><a href="">Ordenadores</a></li>
-                <li><a href="">Componentes</a></li>
-                <li><a href="">Móviles</a></li>
-                <li><a href="">Televisores</a></li>
-                <li><a href="">Hogar tech</a></li>
+           <article className="content">
+           <ul>
+                <li><a href="#" className={selectedCategory === 'Ordenadores' ? 'selected' : ''} onClick={(event) =>
+                    handleCategoryClick('Ordenadores', event)}>Ordenadores</a></li>
+                <li><a href="#" className={selectedCategory === 'Componentes' ? 'selected' : ''} onClick={(event) => 
+                    handleCategoryClick('Componentes', event)}>Componentes</a></li>
+                <li><a href="#" className={selectedCategory === 'Móviles' ? 'selected' : ''} onClick={(event) => 
+                    handleCategoryClick('Móviles', event)}>Móviles</a></li>
+                <li><a href="#" className={selectedCategory === 'Televisores' ? 'selected' : ''} onClick={(event) => 
+                    handleCategoryClick('Televisores', event)}>Televisores</a></li>
+                <li><a href="#" className={selectedCategory === 'Hogar tech' ? 'selected' : ''} onClick={(event) => 
+                    handleCategoryClick('Hogar tech', event)}>Hogar tech</a></li>
             </ul>
-            <div id='carousel-container'>
-            <div id='section-center' ref={carouselRef}>
-            {articles.map((article, index) => (
-                <article key={index}>
-                    <a href={article.link}>
-                        <img src={article.imgSrc} alt={article.title} /> {/* Corregido aquí */}
+            <div id='carousel-container-seleccion'>
+                <div id='section-center-seleccion' ref={carouselRef}>
+                {productsToShow.filter(product => product.category === selectedCategory || 
+                selectedCategory === 'Todos').map((product, index) => (
+                    <article key={index}>
+                        <a href={product.link}>
+                            <img src={product.image} alt={product.title} />
+                        </a>
                         <div>
-                            <h2>{article.title}</h2>
-                            <p>{article.content}</p>
-                            <a href={article.link}>Leer más</a>
+                            <h2>{product.title}</h2>
+                            <p>Precio: {product.price}</p>
+                            <a href={product.link}>Leer más</a>
                         </div>
-                    </a>
-                </article>
-            ))}
-        </div>
-                <div id='buttons'>
-                    <button className="carousel-button-blog" onClick={prevSlide}>←</button>
-                    <button className="carousel-button-blog" onClick={nextSlide}>→</button>
-                </div>
+                    </article>
+                ))}
             </div>
+            <div id='button-center'>
+                        <button className="carousel-button-seleccion" onClick={prevSlide}>←</button>
+                        <button className="carousel-button-seleccion" onClick={nextSlide}>→</button>
+                    </div>
+                </div>
            </article>
         </section>
     )   
